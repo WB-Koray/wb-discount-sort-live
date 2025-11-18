46fb2d65b34361cc3b8297fea453ca1c2c02af09
WB-Koray <koray.welcomebaby@gmail.com>
2025-11-18 13:29:19 +0300

    Update reorder-by-discount.js

--- api/reorder-by-discount.js
+++ api/reorder-by-discount.js
@@ -1,12 +1,1 @@
-// test deploy
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-wb-secret");
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  return res.status(200).json({ ok: true, message: "CORS TEST OK" });
-}
